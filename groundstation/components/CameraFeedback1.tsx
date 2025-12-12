1645288d4ed8465f887de65864b306379c50de29
HrushikeshAnandSarangi <hrushikeshsarangi7@gmail.com>
2025-12-12 19:00:38 +0530

    we will see about that

---
+++ groundstation/components/CameraFeedback1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CameraFeedback1() {
+  return (
+    <div>CameraFeedback1</div>
+  )
+}
---
+++ groundstation/components/CameraFeedback2.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CameraFeedback2() {
+  return (
+    <div>CameraFeedback2</div>
+  )
+}
